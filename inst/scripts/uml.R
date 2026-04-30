4e1e2192b36c8314283d975aefe1fd086f9532e6
Peter Diakumis <peterdiakumis@gmail.com>
2026-04-30 15:37:54 +1000

    refactor uml generation (#51)

--- inst/scripts/uml.R
+++
@@ -1,8 +0,0 @@
-use("nemo", "nemo_uml")
-use("here", "here")
-
-# dput(ls("package:nemo", pattern = "[A-Z].*")) # note: load pkg prior
-nemo_uml(
-  classes = c("Config", "Tool", "Tool1", "Workflow", "Workflow1"),
-  out_dir = here::here("vignettes", "fig", "uml")
-)
