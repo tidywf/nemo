90f4cd4363ee8e37cc37e154b312ef443a19b1ac
pdiakumis <peterdiakumis@gmail.com>
2026-06-07 10:15:09 +1000

    add assert.R with assertion wrappers

---
+++ R/assert.R
@@ -0,0 +1,42 @@
+#' Assert scalar character
+#'
+#' Stops with an informative message if `x` is not a single non-NA character string.
+#' @param x Object to check.
+#' @param arg Name of `x` used in the error message; defaults to the expression
+#'   passed as `x`.
+#' @return `x` invisibly on success.
+#' @export
+nemo_assert_scalar_chr <- function(x, arg = deparse(substitute(x))) {
+  if (!rlang::is_scalar_character(x)) {
+    stop(glue("'{arg}' must be a single character string."), call. = FALSE)
+  }
+  invisible(x)
+}
+
+#' Assert character vector
+#'
+#' Stops with an informative message if `x` is not a character vector.
+#' @param x Object to check.
+#' @param arg Name of `x` used in the error message.
+#' @return `x` invisibly on success.
+#' @export
+nemo_assert_chr <- function(x, arg = deparse(substitute(x))) {
+  if (!rlang::is_character(x)) {
+    stop(glue("'{arg}' must be a character vector."), call. = FALSE)
+  }
+  invisible(x)
+}
+
+#' Assert not NULL
+#'
+#' Stops with an informative message if `x` is `NULL`.
+#' @param x Object to check.
+#' @param arg Name of `x` used in the error message.
+#' @return `x` invisibly on success.
+#' @export
+nemo_assert_not_null <- function(x, arg = deparse(substitute(x))) {
+  if (is.null(x)) {
+    stop(glue("'{arg}' must not be NULL."), call. = FALSE)
+  }
+  invisible(x)
+}
